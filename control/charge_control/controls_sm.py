6f2bbb6c7c9f9243eed3675b126527a90bb5b038
mstv <mstv@gmx.net>
2022-04-28 23:03:36 +0200

    Use max charge if raising

--- control/charge_control/controls_sm.py
+++ control/charge_control/controls_sm.py
@@ -1,7 +1,7 @@
 from data import Controls
 
 
-def _update(current: int, a: int, b: int):
+def _update_mid(current: int, a: int, b: int):
     lower = min(a, b)
     upper = max(a, b)
     changed = current is None or current < lower or upper < current
@@ -10,20 +10,33 @@ def _update(current: int, a: int, b: int):
     return current, changed
 
 
+def _update_mid_max(current: int, a: int, b: int):
+    lower = min(a, b)
+    upper = max(a, b)
+    changed = current is None or current > upper
+    if changed:
+        current = (a + b) // 2
+    else:
+        changed = current < lower
+        if changed:
+            current = upper
+    return current, changed
+
+
 class ControlsSM:
     def __init__(self):
         self._controls = Controls(None, None, None)
 
     def update(self, a: Controls, b: Controls) -> Controls:
-        wallbox_current, wb_changed = _update(self._controls.wallbox_current,
-                                              a.wallbox_current,
-                                              b.wallbox_current)
-        battery_max_discharge, d_changed = _update(self._controls.battery_max_discharge,
-                                                   a.battery_max_discharge,
-                                                   b.battery_max_discharge)
-        battery_max_charge, c_changed = _update(self._controls.battery_max_charge,
-                                                a.battery_max_charge,
-                                                b.battery_max_charge)
+        wallbox_current, wb_changed = _update_mid(self._controls.wallbox_current,
+                                                  a.wallbox_current,
+                                                  b.wallbox_current)
+        battery_max_discharge, d_changed = _update_mid(self._controls.battery_max_discharge,
+                                                       a.battery_max_discharge,
+                                                       b.battery_max_discharge)
+        battery_max_charge, c_changed = _update_mid_max(self._controls.battery_max_charge,
+                                                        a.battery_max_charge,
+                                                        b.battery_max_charge)
         self._controls = Controls(wallbox_current,
                                   battery_max_discharge,
                                   battery_max_charge)
